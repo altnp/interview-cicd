432a363c5f94e04a39d4555c5d0b188b115704fb
Nick Alt <altnp291@gmail.com>
2024-11-13 00:15:06 -0500

    rename tests

--- dotnet-api/Test/EchoTests.cs
+++ dotnet-api/Test/EchoTests.cs
@@ -5,7 +5,7 @@ namespace Test;
 public class UnitTest1
 {
     [Fact]
-    public void Weather()
+    public void EchoHello_WhenProvidedHello()
     {
         var logic = new Logic();
         Assert.Equal("Echo: Hello", logic.Echo("Hello"));
